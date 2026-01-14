2c077fa366659ebf75d2b3211f112db966c4059b
dvrishit <dangeti.rishit@gmail.com>
2026-01-14 11:47:59 +0530

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from flask import Flask, request, jsonify
 from src.predict import predict_one
 
